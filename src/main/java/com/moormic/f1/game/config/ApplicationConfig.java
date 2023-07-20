397c9b312717b30509e8b261ecf614e772258218
moormic <mmoore5411@gmail.com>
2023-07-19 22:20:06 -0400

    Remove redundant config

--- src/main/java/com/moormic/f1/game/config/ApplicationConfig.java
+++ src/main/java/com/moormic/f1/game/config/ApplicationConfig.java
@@ -1,7 +1,5 @@
 package com.moormic.f1.game.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @EnableAutoConfiguration
 @ConfigurationProperties
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ApplicationConfig {
-    @Autowired
-    private final RestClientConfig restClientConfig;
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
